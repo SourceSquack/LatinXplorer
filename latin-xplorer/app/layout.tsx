2fae8a19eb6e37d0fd9ad5ae89022fa71d637263
Zetah07 <sebastiancastro0792@gmail.com>
2023-07-24 17:42:33 -0500

    sign-in and sign-up components are created by CLERK

--- latin-xplorer/app/layout.tsx
+++ latin-xplorer/app/layout.tsx
@@ -1,23 +1,28 @@
-import { Urbanist } from 'next/font/google'
-import type { Metadata } from 'next'
+import { Urbanist } from "next/font/google";
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
 
-import './globals.css'
+import "./globals.css";
 
-const urbanist = Urbanist({ subsets: ['latin'] })
+const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'LatinXplorer - Discover and Explore Tropical Destinations in Latin America',
-  description: 'LatinXplorer is your comprehensive guide to discovering and exploring the most fascinating tropical destinations in Latin America, including Medellín, Mexico City, Buenos Aires, and many more. Plan your trip with detailed information, personalized itineraries, and securely book tour services. Embark on a unique adventure in the lush nature and rich culture of Latin America with LatinXplorer!',
-}
+  title:
+    "LatinXplorer - Discover and Explore Tropical Destinations in Latin America",
+  description:
+    "LatinXplorer is your comprehensive guide to discovering and exploring the most fascinating tropical destinations in Latin America, including Medellín, Mexico City, Buenos Aires, and many more. Plan your trip with detailed information, personalized itineraries, and securely book tour services. Embark on a unique adventure in the lush nature and rich culture of Latin America with LatinXplorer!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={urbanist.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body className={urbanist.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
