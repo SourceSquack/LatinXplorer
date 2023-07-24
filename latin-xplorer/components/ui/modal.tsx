d6fd2dfeee830ed7d25fd3c343956d8fdf3e6e7e
Zetah07 <sebastiancastro0792@gmail.com>
2023-07-24 18:46:41 -0500

    create prisma and first model

---
+++ latin-xplorer/components/ui/modal.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Dialog, DialogHeader, DialogContent, DialogDescription, DialogTitle } from "@/components/ui/dialog";
+
+interface ModalProps {
+    title: string;
+    description: string;
+    isOpen: boolean;
+    onClose: () => void;
+    children?: React.ReactNode;
+};
+
+export const Modal: React.FC<ModalProps> = ({
+    title,
+    description,
+    isOpen,
+    onClose,
+    children
+}) => {
+    const onChange = (open: boolean) => {
+        if (!open) {
+            onClose();
+        }
+    };
+
+    return (
+        <Dialog open={isOpen} onOpenChange={onChange}> 
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription>{description}</DialogDescription>
+                </DialogHeader>
+                <div>
+                    {children}
+                </div>
+            </DialogContent>
+        </Dialog>
+    )
+}
