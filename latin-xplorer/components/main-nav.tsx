abc0fec0da321f967409bdefaf11379d0ece7360
Zetah07 <sebastiancastro0792@gmail.com>
2023-07-24 16:42:45 -0500

    initial prisma creation

--- latin-xplorer/components/main-nav.tsx
+++ latin-xplorer/components/main-nav.tsx
@@ -5,63 +5,61 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 export function MainNav({
-    className, 
-    ...props
-} : React.HTMLAttributes<HTMLDivElement>) {
-    const pathname = usePathname();
-    const params = useParams();
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  const pathname = usePathname();
+  const params = useParams();
 
-    const routes = [
-        {
-            label: "Home",
-            href: "/",
-            active: pathname === "/",
-        },
-        {
-            label: "Destinations",
-            href: "/destinations",
-            active: pathname === "/destinations",
-        },
-        {
-            label: "Stories",
-            href: "/stories",
-            active: pathname === "/stories",
-        },
-        {
-            label: "Reviews",
-            href: "/reviews",
-            active: pathname === "/reviews",
-        },
-        {
-            label: "Contact",
-            href: "/contact",
-            active: pathname === "/contact",
-        },
-        {
-            label: "About",
-            href: "/about",
-            active: pathname === "/about",
-        }
-    ];
+  const routes = [
+    {
+      label: "Home",
+      href: "/",
+      active: pathname === "/",
+    },
+    {
+      label: "Destinations",
+      href: "/destinations",
+      active: pathname === "/destinations",
+    },
+    {
+      label: "Stories",
+      href: "/stories",
+      active: pathname === "/stories",
+    },
+    {
+      label: "Reviews",
+      href: "/reviews",
+      active: pathname === "/reviews",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+      active: pathname === "/contact",
+    },
+    {
+      label: "About",
+      href: "/about",
+      active: pathname === "/about",
+    },
+  ];
 
-    return (
-        <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
-            {
-                routes.map((route) => (
-                    <Link
-                    key={route.href}
-                    href={route.href}
-                    className={
-                        cn(
-                            "text-sm font-medium tansition-colors hover:text-primary",
-                            route.active
-                            ? "text-gray-900 dark:text-white"
-                            : "text-muted-foreground"
-                        )}>
-                        {route.label}
-                        </Link>
-                ))
-            }
-        </nav>
-    );
-} 
+  return (
+    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium tansition-colors hover:text-primary",
+            route.active
+              ? "text-gray-900 dark:text-white"
+              : "text-muted-foreground"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
+}
