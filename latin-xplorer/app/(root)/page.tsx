52aadfef11e3389887b9ae6c66880a2af9b01e8e
Zetah07 <sebastiancastro0792@gmail.com>
2023-07-24 19:42:25 -0500

    create landingPage component // api folder // images source

--- latin-xplorer/app/(root)/page.tsx
+++ latin-xplorer/app/(root)/page.tsx
@@ -1,9 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
-
+import LandingPage from "@/components/ui/landing";
 const SetupPage = () => {
   return (
     <div className="p-4">
+      <>
       <UserButton afterSignOutUrl=""/>
+      </>
+      <LandingPage />
     </div>
   );
 }
