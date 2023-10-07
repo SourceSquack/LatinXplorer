24af90c393722d26fe761cae995f1ebd27460026
Zetah07 <sebastiancastro0792@gmail.com>
2023-10-06 21:19:32 -0500

    authMiddleware change

--- latin-xplorer/middleware.ts
+++ latin-xplorer/middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
+
 export default authMiddleware({
-  // "/" will be accessible to all users
-  publicRoutes: ["/"]
+  publicRoutes: ["/api/:path*"],
 });
- 
+
 export const config = {
-      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
